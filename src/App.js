97b366b52a9f81870f3f68f63b293b600fbb22c8
not_a_coder500 <yashyashsmg@gmail.com>
2023-06-08 20:31:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+function App() {
+  return(
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' Component={Home}/>
+          <Route path='/menu' Component={Menu}/>
+          <Route path='/about' Component={About}/>
+          <Route path='/contact' Component={Contact} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+    
+  );
+}
+
+export default App;
